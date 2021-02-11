24a0170afeec0fb35fa7ee80d904ebc0493a6280
Jubish <jubik4u@gmail.com>
2021-02-11 21:10:14 +0530

    apxe chart which works with stencil webcomponent

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,11 +1,13 @@
 import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
+import ApexCharts from 'apexcharts'
+
 
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true,
+  // shadow: true,
 })
 export class MyComponent {
   /**
@@ -27,7 +29,103 @@ export class MyComponent {
     return format(this.first, this.middle, this.last);
   }
 
+  componentDidLoad(){
+    var options = {
+      chart: {
+        height: 350,
+        type: "line",
+        stacked: false
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ["#FF1654", "#247BA0"],
+      series: [
+        {
+          name: "Series A",
+          data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+        },
+        {
+          name: "Series B",
+          data: [20, 29, 37, 36, 44, 45, 50, 58]
+        }
+      ],
+      stroke: {
+        width: [4, 4]
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: "20%"
+        }
+      },
+      xaxis: {
+        categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+      },
+      yaxis: [
+        {
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#FF1654"
+          },
+          labels: {
+            style: {
+              colors: "#FF1654"
+            }
+          },
+          title: {
+            text: "Series A",
+            style: {
+              color: "#FF1654"
+            }
+          }
+        },
+        {
+          opposite: true,
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#247BA0"
+          },
+          labels: {
+            style: {
+              colors: "#247BA0"
+            }
+          },
+          title: {
+            text: "Series B",
+            style: {
+              color: "#247BA0"
+            }
+          }
+        }
+      ],
+      tooltip: {
+        shared: false,
+        intersect: true,
+        x: {
+          show: false
+        }
+      },
+      legend: {
+        horizontalAlign: "left",
+        offsetX: 40
+      }
+    };
+    
+    var chart = new ApexCharts(document.querySelector("#myChart1"), options);
+    
+    chart.render();
+  }
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>
+      <h1>Test Apex</h1>
+      <div id= "myChart1"></div>
+      </div>;
   }
 }
