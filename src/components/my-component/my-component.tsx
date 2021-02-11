c79b961a930f9cdb578e08d16c0e0d012503dd2a
Jubish <jubik4u@gmail.com>
2021-02-11 23:00:59 +0530

    apex graph added inside shadow dom
    apex graph added inside shadow dom

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,15 +1,18 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h,Element } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 import ApexCharts from 'apexcharts'
 
 
+
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  // shadow: true,
+  shadow: true,
 })
 export class MyComponent {
+
+  @Element() el: HTMLElement;
   /**
    * The first name
    */
@@ -31,6 +34,88 @@ export class MyComponent {
 
   componentDidLoad(){
     var options = {
+      series: [{
+        name: "Session Duration",
+        data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+      },
+      {
+        name: "Page Views",
+        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+      },
+      {
+        name: 'Total Visits',
+        data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+      }
+    ],
+      chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      width: [5, 7, 5],
+      curve: 'straight',
+      dashArray: [0, 8, 5]
+    },
+    title: {
+      text: 'Page Statistics',
+      align: 'left'
+    },
+    legend: {
+      tooltipHoverFormatter: function(val, opts) {
+        console.log("tooltipHoverFormatter");
+        return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+      }
+    },
+    markers: {
+      size: 0,
+      hover: {
+        sizeOffset: 6
+      }
+    },
+    xaxis: {
+      categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+        '10 Jan', '11 Jan', '12 Jan'
+      ],
+    },
+    tooltip: {
+      y: [
+        {
+          title: {
+            formatter: function (val) {
+              return val + " (mins)"
+            }
+          }
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val + " per session"
+            }
+          }
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val;
+            }
+          }
+        }
+      ]
+    },
+    grid: {
+      borderColor: '#f1f1f1',
+    }
+    };
+
+
+    
+    var options2 = {
       chart: {
         height: 350,
         type: "line",
@@ -116,9 +201,13 @@ export class MyComponent {
         offsetX: 40
       }
     };
+
+
     
-    var chart = new ApexCharts(document.querySelector("#myChart1"), options);
+
     
+    var chart = new ApexCharts(this.el.shadowRoot.querySelector("#myChart1"), options);
+    // var chart = new ApexCharts(this.el.querySelector("#myChart1"), options);
     chart.render();
   }
 
@@ -126,6 +215,17 @@ export class MyComponent {
     return <div>
       <h1>Test Apex</h1>
       <div id= "myChart1"></div>
+      <apex-chart
+  type="bar"
+  series={[{
+    name: 'sales',
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+  }]}
+  options={{
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+  }} />
       </div>;
   }
 }
